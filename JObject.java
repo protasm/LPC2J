9697e1b40746205fd59095779f6137a75d3debf6
Protasm <protasm@users.noreply.github.com>
2025-01-09 18:51:24 -0500

    devel/refactor

--- JObject.java
+++
@@ -1,4 +0,0 @@
-class JObject {
-  void foo(int x, String s, float f) {}
-  void bar() { foo(9, "hello", 3.14f); }
-}
---
+++ LPC2J/src/io/github/protasm/lpc2j/LPCObject.java
@@ -0,0 +1,7 @@
+package io.github.protasm.lpc2j;
+
+public class LPCObject {
+    public LPCObject() {
+	System.out.println("Foo");
+    }
+}

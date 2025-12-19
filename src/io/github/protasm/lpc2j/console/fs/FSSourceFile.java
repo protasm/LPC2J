9e550abc0de72f86fc4b4e096fe8051faa9e5296
Jurisware, LLC <devel@jurisware.com>
2025-12-19 18:34:56 -0500

    devel/refactor

--- src/io/github/protasm/lpc2j/console/fs/FSSourceFile.java
+++ src/io/github/protasm/lpc2j/console/fs/FSSourceFile.java
@@ -1,112 +1,106 @@
 package io.github.protasm.lpc2j.console.fs;
 
+import io.github.protasm.lpc2j.parser.ast.ASTObject;
+import io.github.protasm.lpc2j.token.TokenList;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import io.github.protasm.lpc2j.parser.ast.ASTObject;
-import io.github.protasm.lpc2j.token.TokenList;
-
 public class FSSourceFile {
-        private final Path vPath;
-        private String source;
-        private TokenList tokens;
-        private ASTObject astObject;
-        private byte[] bytes;
-        private Object lpcObject;
+  private final Path vPath;
+  private String source;
+  private TokenList tokens;
+  private ASTObject astObject;
+  private byte[] bytes;
+  private Object lpcObject;
 
-    public FSSourceFile(Path vPath) {
-        if (!validExtension(vPath))
-            throw new IllegalArgumentException("Invalid source fileName name.");
+  public FSSourceFile(Path vPath) {
+    if (!validExtension(vPath)) throw new IllegalArgumentException("Invalid source fileName name.");
 
-        this.vPath = vPath;
-    }
+    this.vPath = vPath;
+  }
 
-        public Path vPath() {
-                return vPath;
-        }
+  public Path vPath() {
+    return vPath;
+  }
 
-    public String prefix() {
-        String name = vPath.getFileName().toString();
+  public String prefix() {
+    String name = vPath.getFileName().toString();
 
-        int idx = name.lastIndexOf('.');
+    int idx = name.lastIndexOf('.');
 
-        return name.substring(0, idx);
-    }
+    return name.substring(0, idx);
+  }
 
-    public String extension() {
-        String name = vPath.getFileName().toString();
+  public String extension() {
+    String name = vPath.getFileName().toString();
 
-        int idx = name.lastIndexOf('.');
+    int idx = name.lastIndexOf('.');
 
-        return name.substring(idx + 1);
-    }
+    return name.substring(idx + 1);
+  }
 
-    public Path classPath() {
-        Path parent = vPath.getParent();
+  public Path classPath() {
+    Path parent = vPath.getParent();
 
-        if (parent != null)
-            return Paths.get(parent.toString(), prefix() + ".class");
-        else
-            return Paths.get(prefix() + ".class");
-    }
+    if (parent != null) return Paths.get(parent.toString(), prefix() + ".class");
+    else return Paths.get(prefix() + ".class");
+  }
 
-        public String source() {
-                return source;
-        }
+  public String source() {
+    return source;
+  }
 
-        public void setSource(String source) {
-                this.source = source;
-        }
+  public void setSource(String source) {
+    this.source = source;
+  }
 
-        public TokenList tokens() {
-                return tokens;
-        }
+  public TokenList tokens() {
+    return tokens;
+  }
 
-        public void setTokens(TokenList tokens) {
-                this.tokens = tokens;
-        }
+  public void setTokens(TokenList tokens) {
+    this.tokens = tokens;
+  }
 
-        public ASTObject astObject() {
-                return astObject;
-        }
+  public ASTObject astObject() {
+    return astObject;
+  }
 
-        public void setASTObject(ASTObject astObject) {
-                this.astObject = astObject;
-        }
+  public void setASTObject(ASTObject astObject) {
+    this.astObject = astObject;
+  }
 
-        public byte[] bytes() {
-                return bytes;
-        }
+  public byte[] bytes() {
+    return bytes;
+  }
 
-        public void setBytes(byte[] bytes) {
-                this.bytes = bytes;
-        }
+  public void setBytes(byte[] bytes) {
+    this.bytes = bytes;
+  }
 
-        public Object lpcObject() {
-                return lpcObject;
-        }
+  public Object lpcObject() {
+    return lpcObject;
+  }
 
-        public void setLPCObject(Object lpcObject) {
-                this.lpcObject = lpcObject;
-        }
+  public void setLPCObject(Object lpcObject) {
+    this.lpcObject = lpcObject;
+  }
 
-    public String slashName() {
-        Path parent = vPath.getParent();
+  public String slashName() {
+    Path parent = vPath.getParent();
 
-        if (parent != null)
-            return Paths.get(parent.toString(), prefix()).toString();
-        else
-            return Paths.get(prefix()).toString();
-    }
+    if (parent != null) return Paths.get(parent.toString(), prefix()).toString();
+    else return Paths.get(prefix()).toString();
+  }
 
-    public String dotName() {
-        // replace infix slashes with dots
-        return slashName().replace("/", ".").replace("\\", ".");
-    }
+  public String dotName() {
+    // replace infix slashes with dots
+    return slashName().replace("/", ".").replace("\\", ".");
+  }
 
-    private boolean validExtension(Path vPath) {
-        String name = vPath.getFileName().toString();
+  private boolean validExtension(Path vPath) {
+    String name = vPath.getFileName().toString();
 
-        return (name.endsWith(".lpc") || name.endsWith(".c"));
-    }
+    return (name.endsWith(".lpc") || name.endsWith(".c"));
+  }
 }
--- src/io/github/protasm/lpc2j/console/fs/VirtualFileServer.java
+++ src/io/github/protasm/lpc2j/console/fs/VirtualFileServer.java
@@ -7,136 +7,127 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class VirtualFileServer {
-    private final Path path;
+  private final Path basePath;
 
-    public VirtualFileServer(String basePath) {
-        Path path = Path.of(basePath).normalize();
+  public VirtualFileServer(String basePathStr) {
+    Path basePath = Path.of(basePathStr).normalize();
 
-        if (!path.isAbsolute())
-            throw new IllegalArgumentException("Base path not absolute: " + basePath);
+    if (!basePath.isAbsolute())
+      throw new IllegalArgumentException("Base path not absolute: " + basePathStr);
 
-        this.path = path;
+    this.basePath = basePath;
+  }
+
+  public Path basePath() {
+    return basePath;
+  }
+
+  public Path dirAt(String vPathStr) {
+    if (vPathStr == null) return null;
+    if (vPathStr.isBlank()) return basePath;
+
+    try {
+      Path resolved = resolve(vPathStr, true);
+
+      if ((resolved == null) || !Files.isDirectory(resolved)) return null;
+
+      if (resolved.equals(basePath)) return Path.of("/");
+
+      // Return the portion of the path after base path
+      return basePath.relativize(resolved);
+    } catch (IllegalArgumentException e) {
+      return null;
     }
+  }
 
-    public Path basePath() {
-        return path;
+  public Path fileAt(String vPathStr) {
+    try {
+      if ((vPathStr == null) || vPathStr.isBlank()) throw new IllegalArgumentException();
+
+      Path resolved = resolve(vPathStr, true);
+
+      if ((resolved == null) || !Files.isRegularFile(resolved))
+        throw new IllegalArgumentException();
+
+      return basePath.relativize(resolved);
+    } catch (IllegalArgumentException e) {
+      return null;
     }
+  }
 
-    public Path dirAt(String vPath) {
-        if (vPath == null) return null;
-        if (vPath.isBlank()) return path;
+  public File[] filesIn(String vPathStr) {
+    try {
+      Path resolved = dirAt(vPathStr);
 
-        try {
-            Path resolved = resolve(vPath, true);
+      if (resolved == null) throw new IllegalArgumentException();
 
-            if ((resolved == null) || !Files.isDirectory(resolved))
-                return null;
+      // If resolved is "/", use path directly
+      File dir = resolved.equals(Path.of("/")) ? basePath.toFile() : basePath.resolve(resolved).toFile();
 
-            if (resolved.equals(path))
-                return Path.of("/");
-
-            // Return the portion of the path after base path
-            return path.relativize(resolved);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+      return dir.listFiles();
+    } catch (IllegalArgumentException e) {
+      return null;
     }
+  }
 
-    public Path fileAt(String vPath) {
-        try {
-            if ((vPath == null) || vPath.isBlank())
-                throw new IllegalArgumentException();
+  public boolean read(FSSourceFile sf) {
+    try {
+      Path resolved = resolve(sf.vPath().toString(), true);
 
-            Path resolved = resolve(vPath, true);
+      if ((resolved == null) || !Files.isRegularFile(resolved)) return false;
 
-            if ((resolved == null) || !Files.isRegularFile(resolved))
-                throw new IllegalArgumentException();
+      sf.setSource(Files.readString(resolved));
 
-            return path.relativize(resolved);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+      return true;
+    } catch (IOException e) {
+      return false;
     }
+  }
 
-        public File[] filesIn(String vPathStr) {
-                try {
-                        Path resolved = dirAt(vPathStr);
+  public boolean write(FSSourceFile sf) {
+    try {
+      if (sf.bytes() == null) return false;
 
-            if (resolved == null)
-                throw new IllegalArgumentException();
+      Path target = basePath.resolve(sf.classPath()).normalize();
 
-            // If resolved is "/", use path directly
-            File dir = resolved.equals(Path.of("/")) ? path.toFile() : path.resolve(resolved).toFile();
+      if (!target.startsWith(basePath)) return false;
 
-                        return dir.listFiles();
-                } catch (IllegalArgumentException e) {
-                        return null;
-                }
-        }
+      if (target.getParent() != null) Files.createDirectories(target.getParent());
 
-        public boolean read(FSSourceFile sf) {
-                try {
-                        Path resolved = resolve(sf.vPath().toString(), true);
+      Files.write(target, sf.bytes());
 
-                        if ((resolved == null) || !Files.isRegularFile(resolved))
-                                return false;
-
-                        sf.setSource(Files.readString(resolved));
-
-                        return true;
-                } catch (IOException e) {
-                        return false;
-                }
-        }
-
-        public boolean write(FSSourceFile sf) {
-                try {
-                        if (sf.bytes() == null)
-                                return false;
-
-                        Path target = path.resolve(sf.classPath()).normalize();
-
-                        if (!target.startsWith(path))
-                                return false;
-
-                        if (target.getParent() != null)
-                                Files.createDirectories(target.getParent());
-
-                        Files.write(target, sf.bytes());
-
-                        return true;
-                } catch (IOException | IllegalArgumentException e) {
-                        return false;
-                }
-        }
-
-        public String contentsOfFileAt(String vPathStr) {
-                try {
-                        Path resolved = resolve(vPathStr, true);
-
-                        if ((resolved == null) || !Files.isRegularFile(resolved))
-                                return null;
-
-                        return Files.readString(resolved);
-                } catch (IOException | IllegalArgumentException e) {
-                        return null;
-                }
-        }
-
-        private Path resolve(String vPath, boolean checkExists) {
-        try {
-            // Concatenate base path and vPath, normalize the result
-            Path concat = Paths.get(path.toString(), vPath);
-            Path normalized = concat.normalize();
-
-            // Ensure the normalized path is still within path
-            // and exists (optional).
-            if (!normalized.startsWith(path) || (checkExists && !Files.exists(normalized)))
-                throw new IllegalArgumentException();
-
-            return normalized;
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+      return true;
+    } catch (IOException | IllegalArgumentException e) {
+      return false;
     }
+  }
+
+  public String contentsOfFileAt(String vPathStr) {
+    try {
+      Path resolved = resolve(vPathStr, true);
+
+      if ((resolved == null) || !Files.isRegularFile(resolved)) return null;
+
+      return Files.readString(resolved);
+    } catch (IOException | IllegalArgumentException e) {
+      return null;
+    }
+  }
+
+  private Path resolve(String vPathStr, boolean checkExists) {
+    try {
+      // Concatenate base path and vPathStr, normalize the result
+      Path concat = Paths.get(basePath.toString(), vPathStr);
+      Path normalized = concat.normalize();
+
+      // Ensure the normalized path is still within path
+      // and exists (optional).
+      if (!normalized.startsWith(basePath) || (checkExists && !Files.exists(normalized)))
+        throw new IllegalArgumentException();
+
+      return normalized;
+    } catch (IllegalArgumentException e) {
+      return null;
+    }
+  }
 }

1e3b09d23a76f40d9d54375f63a3ec3c452ff8a3
protasm <36781079+protasm@users.noreply.github.com>
2025-12-20 15:44:12 -0500

    Split IR classes into dedicated files (#64)

--- src/io/github/protasm/lpc2j/ir/IRExpression.java
+++ src/io/github/protasm/lpc2j/ir/IRExpression.java
@@ -2,10 +2,6 @@ package io.github.protasm.lpc2j.ir;
 
 import io.github.protasm.lpc2j.runtime.RuntimeType;
 
-public sealed interface IRNode permits IRExpression, IRStatement {
-    int line();
-}
-
 public sealed interface IRExpression extends IRNode
         permits IRBinaryOperation,
                 IRCoerce,
@@ -20,7 +16,3 @@ public sealed interface IRExpression extends IRNode
                 IRUnaryOperation {
     RuntimeType type();
 }
-
-public sealed interface IRStatement extends IRNode permits IRConditionalJump, IRExpressionStatement, IRJump, IRReturn {}
-
-public sealed interface IRTerminator extends IRStatement permits IRConditionalJump, IRJump, IRReturn {}

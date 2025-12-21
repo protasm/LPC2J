566ac550bc4b833488120eb19dfc45869a86f8f4
protasm <36781079+protasm@users.noreply.github.com>
2025-12-21 11:35:46 -0500

    Initialize field values in generated classes (#84)

--- src/io/github/protasm/lpc2j/ir/IRField.java
+++ src/io/github/protasm/lpc2j/ir/IRField.java
@@ -1,9 +1,10 @@
 package io.github.protasm.lpc2j.ir;
 
 import io.github.protasm.lpc2j.runtime.RuntimeType;
+import io.github.protasm.lpc2j.ir.IRExpression;
 import java.util.Objects;
 
-public record IRField(int line, String name, RuntimeType type) {
+public record IRField(int line, String name, RuntimeType type, IRExpression initializer) {
     public IRField {
         Objects.requireNonNull(name, "name");
         Objects.requireNonNull(type, "type");

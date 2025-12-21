caf4096e3e6b2512f12e0a99a7795612e7e87ba6
Jurisware Developer <devel@jurisware.com>
2025-12-21 10:10:34 -0500

    devel/refactor

--- src/io/github/protasm/lpc2j/console/cmd/CmdHelp.java
+++ src/io/github/protasm/lpc2j/console/cmd/CmdHelp.java
@@ -5,7 +5,11 @@ import io.github.protasm.lpc2j.console.LPCConsole;
 public class CmdHelp extends Command {
     @Override
     public boolean execute(LPCConsole console, String... args) {
-        System.out.println("[pp] preprocess -> [s] scan -> [p] parse -> [a] analyze -> [c] compile -> [l] load");
+        System.out.println("===========================================");
+        System.out.println("[pp] preprocess -> [s] scan -> [p] parse ->");
+        System.out.println("[a] analyze -> [c] compile -> [l] load");
+        System.out.println("===========================================\n");
+
         LPCConsole.commands().forEach((cmd, aliases) -> {
             System.out.printf("%-20s -> %s%n", String.join(", ", aliases), cmd);
         });
